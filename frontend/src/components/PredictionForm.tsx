d5f137229cc03c2e8034fd54516323d03675b6bb
Muhammad Mizan Al Mujadid <mizanmujadid2436@gmail.com>
2025-12-22 22:21:34 +0700

    refactor: unused import on PrefictionForm.tsx

--- frontend/src/components/PredictionForm.tsx
+++ frontend/src/components/PredictionForm.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from "react";
+import { useMemo, useState } from "react";
 import type { FormEvent } from "react";
 import { Map, Marker } from "pigeon-maps";
 import type { PredictionInput, PredictionResponse } from "../types/prediction";
